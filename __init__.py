07e82623e1099299ebe4e1a128215f372382eaa6
Sobirov Og‘abek <76460501+ddasturbek@users.noreply.github.com>
2024-04-24 18:32:04 +0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
+"""
+    Muallif: Dasturbek (github.com/ddasturbek)
+    Versiya: 1.0.0
+
+    Dasturdan o‘zbek tilidagi matnlarni yozuv shaklini almashtirishda
+    va yangi lotin alifbosini joriy qilishda foydalanish mumkin!
+
+"""
+
 from LotinKrillYangiLotin.LotinKrillYangiLotin import Almashtirish
