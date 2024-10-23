64253a796a53ad57085e4b62b0ca76ab5d34f1cb
Sobirov Og‘abek <76460501+ddasturbek@users.noreply.github.com>
2024-10-23 14:41:23 +0500

    Update LotinKrillYangilotin.py

--- LotinKrillYangilotin.py
+++ LotinKrillYangilotin.py
@@ -1,3 +1,27 @@
+"""
+    Muallif: Dasturbek (github.com/ddasturbek)
+    Versiya: 1.0.0
+
+    Dasturdan o‘zbek tilidagi matnlarni yozuv shaklini almashtirishda
+    va yangi lotin alifbosini joriy qilishda foydalanish mumkin!
+
+    O‘zbek Lotin alifbosida 29 harf va bitta tutuq belgi (’) bor.
+    O‘zbek Krill alifbosida shunga mos, ya’ni 30 ta harf va belgi bor.
+    O‘zbek Yangi lotin alifbosida ham jami 30 ta harf va belgi bor.
+
+    Alifbo bilan to‘liq holda quyida tanishishingiz mumkin:
+    https://github.com/ddasturbek/LotinKrillYangiLotin/assets/76460501/a36715a4-2108-4179-b127-0409c5525708
+
+    Yangi lotin yozuvi haqida O‘zbekiston Respublikasi Fanlar akademiyasi tomonidan 'NORMATIV-HUQUQIY HUJJATLAR LOYIHALARI MUHOKAMASI PORTALI'ga muhokamaga qo‘yilgan:
+    https://regulation.gov.uz/oz/d/31596
+
+    Harflarni almashtirishda foydalanilgan qoidalar manbalari:
+    1. https://uz.wikipedia.org/wiki/Vikipediya:O%CA%BBzbek_lotin_alifbosi_qoidalari
+    2. https://uz.wikipedia.org/wiki/Vikipediya:Imlo_va_grammatika
+
+"""
+
+
 class Almashtirish:
 
     def __init__(self):
